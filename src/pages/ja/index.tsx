3393decfd58752a1b57f43bb1c603e480c0bfd08
luke2327 <qwepoi8526@gmail.com>
2022-05-30 17:42:12 +0900

    mod: remove empty pages

--- src/pages/ja/index.tsx
+++ src/pages/ja/index.tsx
@@ -1,2 +1,13 @@
 import { Box, Typography } from '@mui/material';
 
+const JAIndex = () => {
+  return (
+    <Box>
+      <Typography>
+        JA
+      </Typography>
+    </Box>
+  )
+}
+
+export default JAIndex;
